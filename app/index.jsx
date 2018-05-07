9e46aafa6dbf431a19ad263969ab8c6345ec1d76
Rigwarl <Avips@mail.ru>
2018-05-08 00:50:21 +0300

    add random name generation

--- app/index.jsx
+++ app/index.jsx
@@ -2,9 +2,19 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import faker from 'faker';
+import cookies from 'js-cookie';
 import React from 'react';
 import { render } from 'react-dom';
+
 import App from './App';
+
+const nameFromCookies = cookies.get('name');
+const name = nameFromCookies || faker.name.findName();
+
+if (!nameFromCookies) {
+  cookies.set('name', name);
+}
 
 const { channels } = window.gon;
 const root = document.querySelector('#root');
