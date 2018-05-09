2f8f9143ef1a042fa2219726d8fc421aafdd00cb
Rigwarl <Avips@mail.ru>
2018-05-09 16:13:15 +0300

    make state exact

--- app/types/index.js
+++ app/types/index.js
@@ -1,18 +1,19 @@
 // @flow
 
-export type User = {
-  name: string,
-}
+export type User = {|
+  +name: string,
+|};
 
-export type Channel = {
+export type Channel = {|
   +id: number,
   +name: string,
   +removable: boolean,
-};
+|};
 
-export type State = {
+export type State = {|
   +user: User,
   +channels: {|
     +[string]: Channel
   |},
-};
+  +activeChannelId: number,
+|};
