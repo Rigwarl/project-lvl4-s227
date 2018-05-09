eed97dad6b8e011b8a58f6b8c9af02bf5d9374fc
Rigwarl <Avips@mail.ru>
2018-05-09 19:26:07 +0300

    move work with cookies to index

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,18 +1,5 @@
 // @flow
 
-import faker from 'faker';
-import cookies from 'js-cookie';
 import { createAction } from 'redux-actions';
 
-export const initApp = createAction('APP_INIT', (data) => {
-  const nameFromCookies = cookies.get('userName');
-  const name = nameFromCookies || faker.name.findName();
-
-  if (!nameFromCookies) {
-    cookies.set('userName', name);
-  }
-
-  const user = { name };
-
-  return { ...data, user };
-});
+export const initApp = createAction('APP_INIT');
--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import gon from 'gon';
+import faker from 'faker';
+import cookies from 'js-cookie';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
@@ -17,13 +19,23 @@ if (root === null) {
   throw new Error('no #root element');
 }
 
+const nameFromCookies = cookies.get('userName');
+const name = nameFromCookies || faker.name.findName();
+
+if (!nameFromCookies) {
+  cookies.set('userName', name);
+}
+
+const user = { name };
+const data = { ...gon, user };
+
 /* eslint-disable no-underscore-dangle */
 const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
 const devtoolMiddleware = ext && ext();
 /* eslint-enable */
 
 const store = createStore(reducers, devtoolMiddleware);
-store.dispatch(initApp(gon));
+store.dispatch(initApp(data));
 
 render(
   <Provider store={store}>
