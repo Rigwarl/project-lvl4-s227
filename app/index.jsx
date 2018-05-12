405cf755eabdd3c20da6db43f44cf95de9ec8e4a
Rigwarl <Avips@mail.ru>
2018-05-12 22:12:34 +0300

    move creating store to store.js

--- app/index.jsx
+++ app/index.jsx
@@ -2,42 +2,18 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import gon from 'gon';
-import faker from 'faker';
-import cookies from 'js-cookie';
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reducers from './reducers';
-import * as actions from './actions';
 import App from './components/App';
-import { connect } from './server';
+import createStore from './store';
 
 const root = document.getElementById('root');
+const store = createStore();
 
 if (root === null) {
   throw new Error('no #root element');
 }
-
-const nameFromCookies = cookies.get('userName');
-const name = nameFromCookies || faker.name.findName();
-
-if (!nameFromCookies) {
-  cookies.set('userName', name);
-}
-
-const user = { name };
-const data = { ...gon, user };
-
-const store = createStore(
-  reducers,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
-store.dispatch(actions.initApp(data));
-connect(store.dispatch, actions);
 
 render(
   <Provider store={store}>
---
+++ app/store.js
@@ -0,0 +1,30 @@
+// @flow
+
+import gon from 'gon';
+import faker from 'faker';
+import cookies from 'js-cookie';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers';
+import * as actions from './actions';
+import { connect } from './server';
+
+export default () => {
+  const nameFromCookies = cookies.get('userName');
+  const name = nameFromCookies || faker.name.findName();
+
+  if (!nameFromCookies) {
+    cookies.set('userName', name);
+  }
+
+  const user = { name };
+  const data = { ...gon, user };
+  const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunk)),
+  );
+
+  store.dispatch(actions.initApp(data));
+  connect(store.dispatch, actions);
+};
