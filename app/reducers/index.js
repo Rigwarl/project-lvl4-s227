9223367ddc21f9a9c23a990bb8b7667f3e6b1e8c
Rigwarl <Avips@mail.ru>
2018-05-15 22:12:44 +0300

    add types for reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,24 +1,65 @@
+// @flow
+
 import _ from 'lodash';
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import { handleActions } from 'redux-actions';
+import { handleActions, type ActionType } from 'redux-actions';
 import * as actions from '../actions';
+import type { ChannelsMap, MessagesMap, User } from '../types';
 
 const user = handleActions({
-  [actions.initApp]: (state, { payload: { user: { name } } }) => ({ ...state, name }),
+  [actions.initApp.toString()](
+    state: User,
+    action: ActionType<typeof actions.initApp>,
+  ): User {
+    const { payload: { user: { name } } } = action;
+
+    return { ...state, name };
+  },
 }, { name: '' });
 
 const channels = handleActions({
-  [actions.initApp]: (state, { payload }) => _.keyBy(payload.channels, 'id'),
+  [actions.initApp.toString()](
+    state: ChannelsMap,
+    action: ActionType<typeof actions.initApp>,
+  ): ChannelsMap {
+    const { payload } = action;
+    const channelsById = _.keyBy(payload.channels, 'id');
+
+    return { ...channelsById };
+  },
 }, {});
 
 const messages = handleActions({
-  [actions.initApp]: (state, { payload }) => _.keyBy(payload.messages, 'id'),
-  [actions.addMessageEvent]: (state, { payload }) => ({ ...state, [payload.id]: payload }),
+  [actions.initApp.toString()](
+    state: MessagesMap,
+    action: ActionType<typeof actions.initApp>,
+  ): MessagesMap {
+    const { payload } = action;
+    const messagesById = _.keyBy(payload.messages, 'id');
+
+    return { ...messagesById };
+  },
+
+  [actions.addMessageEvent.toString()](
+    state: MessagesMap,
+    action: ActionType<typeof actions.addMessageEvent>,
+  ): MessagesMap {
+    const { payload } = action;
+
+    return { ...state, [payload.id]: payload };
+  },
 }, {});
 
 const currentChannelId = handleActions({
-  [actions.initApp]: (state, { payload }) => payload.currentChannelId,
+  [actions.initApp.toString()](
+    state: number,
+    action: ActionType<typeof actions.initApp>,
+  ) {
+    const { payload } = action;
+
+    return payload.currentChannelId;
+  },
 }, 0);
 
 export default combineReducers({
