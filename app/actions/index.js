635d76c1c413d137f1ddda4c79d59d9964c20775
Rigwarl <Avips@mail.ru>
2018-05-15 22:12:25 +0300

    fix init action type

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,9 +2,9 @@
 
 import { createAction } from 'redux-actions';
 import { request } from '../server';
-import type { State, NewMessage, Message } from '../types';
+import type { InitData, NewMessage, Message } from '../types';
 
-export const initApp = createAction('APP_INIT', (state: State) => state);
+export const initApp = createAction('APP_INIT', (data: InitData) => data);
 
 export const addMessageEvent = createAction('MESSAGE_ADD_EVENT', (message: Message) => message);
 
