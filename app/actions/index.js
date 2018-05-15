f10da149d069f9b7d1610e4e9ed943333aab488c
Rigwarl <Avips@mail.ru>
2018-05-15 17:55:32 +0300

    fix actions types

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,9 +2,10 @@
 
 import { createAction } from 'redux-actions';
 import { request } from '../server';
+import type { State, NewMessage, Message } from '../types';
 
-export const initApp = createAction('APP_INIT');
+export const initApp = createAction('APP_INIT', (state: State) => state);
 
-export const addMessageEvent = createAction('MESSAGE_ADD_EVENT');
+export const addMessageEvent = createAction('MESSAGE_ADD_EVENT', (message: Message) => message);
 
-export const addMessage = message => () => request.addMessage(message);
+export const addMessage = (message: NewMessage) => () => request.addMessage(message);
