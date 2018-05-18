796f0152f5a50b4006487de6a0e1eee83aaa9d82
Rigwarl <Avips@mail.ru>
2018-05-18 12:28:17 +0300

    add RemoveChannelPopup

--- app/components/Popup.jsx
+++ app/components/Popup.jsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import NewChannelPopup from './NewChannelPopup';
+import RemoveChannelPopup from './RemoveChannelPopup';
 import type { State, PopupName } from '../types';
 
 type Props = {|
@@ -17,8 +18,8 @@ const Popup = ({ popupName }: Props) => {
   switch (popupName) {
     case 'newChannel':
       return <NewChannelPopup />;
-    // case 'removeChannel':
-    //   return <RemoveChannelPopup />;
+    case 'removeChannel':
+      return <RemoveChannelPopup />;
     // case 'editChannel':
     //   return <EditChannelPopup />;
     case 'none':
---
+++ app/components/RemoveChannelPopup.jsx
@@ -0,0 +1,56 @@
+// @flow
+
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import * as actions from '../actions';
+import type { State, Channel } from '../types';
+
+type Props = {|
+  opened: boolean,
+  channel: Channel,
+|};
+
+type DispatchProps = {
+  closePopup: typeof actions.closePopup,
+  removeChannel: typeof actions.removeChannel,
+};
+
+const mapStateToProps = (state: State): Props => {
+  if (typeof state.popup.data !== 'number') {
+    throw new Error('Remove Channel Popup need channelId as data prop');
+  }
+
+  return {
+    opened: state.popup.open,
+    channel: state.channels[state.popup.data],
+  };
+};
+
+const dispatchProps: DispatchProps = actions;
+
+class RemoveChannelPopup extends React.Component<Props & DispatchProps> {
+  onRemove = async () => {
+    await this.props.removeChannel(this.props.channel.id);
+    this.props.closePopup();
+  }
+
+  render() {
+    return (
+      <Modal isOpen={this.props.opened} toggle={this.props.closePopup}>
+        <ModalHeader toggle={this.props.closePopup}>
+          Remove channel {this.props.channel.name}?
+        </ModalHeader>
+        <ModalBody>
+          You will not be able to restore it!
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" onClick={this.onRemove}>Remove</Button>
+          <Button color="secondary" onClick={this.props.closePopup}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+export default connect(mapStateToProps, dispatchProps)(RemoveChannelPopup);
