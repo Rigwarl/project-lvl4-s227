5963ac8d46c83b577641d13e0ef0d6d0c37ef4fa
Rigwarl <Avips@mail.ru>
2018-05-29 12:51:08 +0300

    add form and submitting status to removeChannelPopup

--- app/components/Popup/RemoveChannelPopup.jsx
+++ app/components/Popup/RemoveChannelPopup.jsx
@@ -2,7 +2,8 @@
 
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { reduxForm, type FormProps } from 'redux-form';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Form } from 'reactstrap';
 import * as actions from '../../actions';
 import type { State, Channel } from '../../types';
 
@@ -29,7 +30,7 @@ const mapStateToProps = (state: State): Props => {
 
 const dispatchProps: DispatchProps = actions;
 
-class RemoveChannelPopup extends React.Component<Props & DispatchProps> {
+class RemoveChannelPopup extends React.Component<Props & DispatchProps & FormProps> {
   onRemove = () => this.props.removeChannel(this.props.channel.id)
 
   onClose = () => this.props.freeChannel(this.props.channel.id)
@@ -41,15 +42,30 @@ class RemoveChannelPopup extends React.Component<Props & DispatchProps> {
           Remove channel {this.props.channel.name}?
         </ModalHeader>
         <ModalBody>
-          You will not be able to restore it!
+          <Form id="channel-remove-form" onSubmit={this.props.handleSubmit(this.onRemove)}>
+            You will not be able to restore it!
+          </Form>
         </ModalBody>
         <ModalFooter>
-          <Button color="danger" onClick={this.onRemove}>Remove</Button>
-          <Button color="secondary" onClick={this.onClose}>Cancel</Button>
+          <Button
+            disabled={this.props.submitting}
+            form="channel-remove-form"
+            type="submit"
+            color="danger"
+          >Remove
+          </Button>
+          <Button
+            disabled={this.props.submitting}
+            color="secondary"
+            onClick={this.onClose}
+          >Cancel
+          </Button>
         </ModalFooter>
       </Modal>
     );
   }
 }
 
-export default connect(mapStateToProps, dispatchProps)(RemoveChannelPopup);
+export default connect(mapStateToProps, dispatchProps)(reduxForm({
+  form: 'removeChannel',
+})(RemoveChannelPopup));
