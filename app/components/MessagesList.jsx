0d74098e97054752bf43cabfaa207af7ffcb2db7
Rigwarl <Avips@mail.ru>
2018-06-08 17:31:25 +0300

    fix messages scroll height

--- app/components/MessagesList.jsx
+++ app/components/MessagesList.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
+import cn from 'classnames';
 import { messagesSelector } from '../selectors';
 import type { State, Message } from '../types';
 
@@ -14,17 +15,23 @@ const mapStateToProps = (state: State): Props => ({
   messages: messagesSelector(state),
 });
 
-const renderMessageListItem = ({ id, text, userName }) => (
-  <Card key={id} className="mb-4">
-    <CardBody>
-      <CardTitle>{userName}</CardTitle>
-      <CardText>{text}</CardText>
-    </CardBody>
-  </Card>
-);
+const renderMessageListItem = ({ id, text, userName }, i, messages) => {
+  const className = cn({
+    'mb-4': i !== messages.length - 1,
+  });
+
+  return (
+    <Card key={id} className={className}>
+      <CardBody>
+        <CardTitle>{userName}</CardTitle>
+        <CardText>{text}</CardText>
+      </CardBody>
+    </Card>
+  );
+};
 
 const MessagesList = ({ messages }: Props) => (
-  <div className="messages-list">
+  <div className="messages-list mb-4">
     {messages.map(renderMessageListItem)}
   </div>
 );
